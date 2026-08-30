44febbf995ce45ab5990eb182ae6e95d59900499
NeerajSangwan <neerajjitender777@gmail.com>
2026-08-30 13:23:25 +0530

    async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,22 @@ import { loadProducts, loadProductsFetch } from "../data/products.js";
 // import "../data/cart-class.js";
 import "../data/backend-practice.js";
 
+/*
+async function loadPage() {
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+*/
+
 new Promise((resolve) => {
   loadProducts(() => {
     resolve();
